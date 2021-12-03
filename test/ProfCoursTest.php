727f8bb3048db2cd2051ba40942131abedd4a187
RomainMONNOYER <Romain.MONNOYER@student.umons.ac.be>
2021-12-03 14:47:07 +0100

    test updateOne

--- test/ProfCoursTest.php
+++ test/ProfCoursTest.php
@@ -346,7 +346,12 @@ class ProfCoursTest extends TestCase
         $this->assertTrue($val, "Update du prof num $idProf ...\n");
 
         // Cours
-        
+        $cours = new Cours($this->intitule, $this->duree);
+        $val = $cours->updateOne($conn, $idCours);
+        $expected_cours_str = $cours->__toString();
+        $record_cours = Cours::printOne($conn, $idCours);
+        $this->assertEquals($expected_cours_str, $record_cours->__toString(), "Update du cours $idCours ...\n");
+        $this->assertTrue($val, "Update du cours num $idCours ...\n");
         /**
         *
         * Question 11 :	Dans la fonction « testUpdateOne() », 
